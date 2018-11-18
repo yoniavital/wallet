d699276893a000f54fa8b583658e8940f90850ce
avitaljonatan <avital.jonatan@gmail.com>
2018-11-18 16:05:15 -0500

    build onboarding pages

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,33 @@
+// Global variables
+var onboardingInterval;
+
 $(document).ready(function(){
   $( "#body_container" ).load( "components/welcome.html");
 });
+
+function goTo(pageName) {
+  $( "#body_container" ).load( "components/" + pageName);
+
+  // This function builds the onboarging list items
+  onboardingInterval = setInterval(createOnboardingList, 5);
+}
+
+function createOnboardingList() {
+  list = $(".onboaring-list");
+  if(list.length > 0) {
+    $(".onboaring-list").slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+
+    clearInterval(onboardingInterval);
+  }
+}
+
+function goNextSlide() {
+  $(".onboaring-list").slick('slickNext');
+}
