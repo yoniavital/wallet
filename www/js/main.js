c6bc9a03b44fc45979aaa50b02988c5729c36ff9
avitaljonatan <avital.jonatan@gmail.com>
2018-12-04 14:58:48 -0500

    refactor onboarding sliding cards

--- www/js/main.js
+++ www/js/main.js
@@ -2,45 +2,53 @@
 var onboardingInterval;
 var currentSlickSlide = 0;
 
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ){
+    if ( ns.includes("noPreventDefault") ) {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } else {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
+
 $(document).ready(function(){
   $("body").children().each(function() {
     $(this).html($(this).html().replace(/&#8232;/g," "));
   });
+  var currentPage = document.location.href.match(/[^\/]+$/)[0];
+  if (currentPage === "onboarding.html") {
+    var mySwiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+      },
+      on: {
+        reachEnd: function () {
+          $("#submit_btn").attr("onclick", "exitTour()");
+          $("#submit_btn").text("Finish Tour");
+        },
+        slidePrevTransitionEnd: function () {
+          $("#submit_btn").attr("onclick", "goNextSlide()");
+          $("#submit_btn").text("Next")
+        },
+      }
+    });
+  }
 });
 
-function createOnboardingList() {
-  list = $(".onboaring-list");
-  if(list.length > 0) {
-    $(".onboaring-list").slick({
-      dots: true,
-      infinite: false,
-      arrows: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
+function buildSwiperSlides() {
 
-    clearInterval(onboardingInterval);
-  }
 }
 
-  $(this).on('afterChange', function(event, slick, currentSlide) {
-    currentSlickSlide = currentSlide;
-  if (currentSlide === 2) {
-    $("#submit_btn").attr("onclick","exitTour()");
-    $("#submit_btn").text("Finish Tour");
-  } else {
-    $("#submit_btn").attr("onclick","goNextSlide()");
-    $("#submit_btn").text("Next")
-  }
-})
-
 function goNextSlide() {
-  $(".onboaring-list").slick('slickNext');
+  var mySwiper = document.querySelector('.swiper-container').swiper;
+  mySwiper.slideNext();
 }
 
 function goPreviousSlide() {
-  $(".onboaring-list").slick('slickPrev');
+  var mySwiper = document.querySelector('.swiper-container').swiper;
+  mySwiper.slidePrev();
 }
 
 function pageBack() {
@@ -50,7 +58,8 @@ function pageBack() {
       window.location.href = "mobilechain://exitwallet";
       break;
     case "onboarding.html":
-      if (currentSlickSlide > 0) {
+      var mySwiper = document.querySelector('.swiper-container').swiper;
+      if (mySwiper.activeIndex > 0) {
         goPreviousSlide()
       } else {
         window.location.href = "welcome.html";
